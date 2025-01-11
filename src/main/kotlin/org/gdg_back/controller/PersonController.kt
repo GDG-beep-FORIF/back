dacb2be0c9a1c8af94b2805569dbf81d7fccd2eb
poqo <zxvm5962@naver.com>
2025-01-12 06:07:15 +0900

    feat: Add getPersonInfo by person id

--- src/main/kotlin/org/gdg_back/controller/PersonController.kt
+++ src/main/kotlin/org/gdg_back/controller/PersonController.kt
@@ -5,11 +5,12 @@ import org.gdg_back.service.PersonService
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
-
+import java.util.*
 
 @RestController
 @RequestMapping("/persons")
@@ -20,5 +21,10 @@ class PersonController(
     @GetMapping
     fun getPersonInfo(@RequestParam name: String): ResponseEntity<PersonResponse> {
         return ResponseEntity(personService.getPersonInfo(name), HttpStatus.OK)
+    }
+
+    @GetMapping("/{id}")
+    fun getPersonById(@PathVariable id: UUID): ResponseEntity<PersonResponse> {
+        return ResponseEntity(personService.getPersonInfo(id), HttpStatus.OK)
     }
 }
--- src/main/kotlin/org/gdg_back/service/PersonService.kt
+++ src/main/kotlin/org/gdg_back/service/PersonService.kt
@@ -4,6 +4,7 @@ import jakarta.persistence.EntityNotFoundException
 import org.gdg_back.dto.PersonResponse
 import org.gdg_back.repository.BasicInfoRepository
 import org.springframework.stereotype.Service
+import java.util.UUID
 
 
 @Service
@@ -18,6 +19,12 @@ class PersonService (
 
         val person = basicInfoRepository.findByName(name) ?: throw EntityNotFoundException("Person not found")
 
+        return PersonResponse.from(person)
+    }
+
+    fun getPersonInfo(id: UUID): PersonResponse {
+        val person = basicInfoRepository.findById(id)
+            .orElseThrow { EntityNotFoundException("Person not found") }
         return PersonResponse.from(person)
     }
 
