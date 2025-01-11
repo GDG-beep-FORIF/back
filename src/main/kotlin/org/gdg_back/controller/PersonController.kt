e0948ba202c9ea34246553fd766a2ec44b6eafc0
poqo <zxvm5962@naver.com>
2025-01-12 01:30:48 +0900

    chore: Delete unused import

--- src/main/kotlin/org/gdg_back/controller/PersonController.kt
+++ src/main/kotlin/org/gdg_back/controller/PersonController.kt
@@ -3,7 +3,6 @@ package org.gdg_back.controller
 import org.gdg_back.dto.PersonResponse
 import org.gdg_back.service.PersonService
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
